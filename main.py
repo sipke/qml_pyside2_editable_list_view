02128b6de759046df06f0a965d039d919bec0781
Sipke Vriend <>
2020-04-08 13:30:59 +1000

    Add a python thread/timer to demonstrate that updating listview elements does not work from them.

--- main.py
+++ main.py
@@ -15,4 +15,24 @@ class MainWindow(QQuickView):
 myApp = QApplication(sys.argv)
 ui = MainWindow()
 ui.show()
+
+"""
+Add a python thread/timer to demonstrate that updating listview elements does not work from them.
+"""
+import threading
+age = 0
+def update():
+    print("update in timer\n")
+    global age, timer
+    import time
+    for age in range(5):
+        # Unless you re-size the window, these additions will not be updated in QML UI.
+        ui.model.addPerson("older me", 20 + age)
+        time.sleep(2)
+    ui.model.addPerson("last one", 40)
+
+
+timer = threading.Timer(1.0, update)
+timer.start()
+
 sys.exit(myApp.exec_())
